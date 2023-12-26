00c1fcac07ea57466773875c826d112dc7e5c14d
Raed-93 <tbilhraed@gmail.com>
2023-12-26 12:03:27 +0200

    first_java

---
+++ Java Fundamentals/FirstJavaprogram.java
@@ -0,0 +1,9 @@
+public class FirstJavaprogram{
+    public static void main(String[] args){
+        System.out.println("My name is Raed");
+        System.out.println("I am 30 yars old");
+        System.out.println("My HomeTown is Nablus");
+
+    }
+    
+}
