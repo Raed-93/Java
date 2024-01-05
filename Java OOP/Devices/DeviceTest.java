215348142495e6eeee08fcbfcd20324d95d869d2
Raed-93 <tbilhraed@gmail.com>
2024-01-05 23:58:11 +0200

    phone

---
+++ Java OOP/Devices/DeviceTest.java
@@ -0,0 +1,15 @@
+public class DeviceTest {
+    
+    public static void main(String[] args) {
+        Phone p = new Phone();
+
+        p.makeCall();
+        p.makeCall();
+        p.makeCall();
+
+        p.playGame();
+        p.playGame();
+
+        p.charge();
+    }
+}
